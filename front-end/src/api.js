7726200fa50f68b1a25811dcd5a3f0953ce8b95f
Group-Project05 <Sahilprojectwork0214@gmail.com>
2026-04-13 11:17:13 +0530

    Update api.js

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({baseURL: "http://localhost:3000"});
+const api = axios.create({baseURL: "https://your-backend.onrender.com"});
 
 api.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
