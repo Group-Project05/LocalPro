179d52d165b3f970fdb704099b732ee3fee921a9
Group-Project05 <sahilprojectWork0214@gmail.com>
2026-04-14 14:38:36 +0530

    fix send email

--- back-end/utils/sendMail.js
+++ back-end/utils/sendMail.js
@@ -3,20 +3,27 @@ const nodemailer = require("nodemailer");
 const sendMail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    pool: true,
     auth: {
       user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS
+      pass: process.env.EMAIL_PASS,
     },
     tls: {
-    rejectUnauthorized: false 
-  }
+      rejectUnauthorized: false,
+      connectionTimeout: 10000, // 10 seconds
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
+    },
   });
 
   await transporter.sendMail({
     from: `"Smart Service" <${process.env.EMAIL}>`,
     to,
     subject,
-    text
+    text,
   });
 };
 
