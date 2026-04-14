ecf5ce04aa27e0e1141ead34c95ccad8a2dc83cd
Group-Project05 <sahilprojectWork0214@gmail.com>
2026-04-14 15:54:11 +0530

    fix send email

--- back-end/utils/sendMail.js
+++ back-end/utils/sendMail.js
@@ -1,22 +1,28 @@
 const { Resend } = require('resend');
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (to, subject, text) => {
   try {
-    const data = await resend.emails.send({
+    const response = await resend.emails.send({
       from: 'LocalPro <onboarding@resend.dev>',
-      to: to,
+      to: to, // Yaad rahe: Testing mein wahi email daalein jisse Resend signup kiya hai
       subject: subject,
       text: text,
     });
 
-    console.log("Email sent successfully via Resend:", data.id);
+    // Resend ka naya version { data, error } return karta hai
+    const { data, error } = response;
+
+    if (error) {
+      console.error("Resend API Error:", error);
+      throw new Error(error.message);
+    }
+
+    console.log("Email sent successfully! ID:", data.id);
     return data;
+
   } catch (error) {
-    console.error("Resend Error:", error);
+    console.error("Internal sendMail Error:", error.message);
     throw error;
   }
 };
-
-module.exports = sendMail;
