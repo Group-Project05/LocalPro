772dfada42e2f9fcf757f3615bf12f806059918b
Group-Project05 <sahilprojectWork0214@gmail.com>
2026-04-14 15:05:26 +0530

    fix send email

--- back-end/utils/sendMail.js
+++ back-end/utils/sendMail.js
@@ -4,27 +4,33 @@ const sendMail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     pool: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 20000,
     tls: {
       rejectUnauthorized: false,
-      connectionTimeout: 10000, // 10 seconds
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     },
   });
 
-  await transporter.sendMail({
-    from: `"Smart Service" <${process.env.EMAIL}>`,
-    to,
-    subject,
-    text,
-  });
+  try {
+    await transporter.sendMail({
+      from: `"Smart Service" <${process.env.EMAIL}>`,
+      to,
+      subject,
+      text,
+    });
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Nodemailer Internal Error:", error);
+    throw error;
+  }
 };
 
 module.exports = sendMail;
