7ff7b157307c74a184f09cc4675bc234c21673df
Group-Project05 <sahilprojectWork0214@gmail.com>
2026-04-15 11:11:23 +0530

    Fix after edit service navigate

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const api = axios.create({baseURL: "https://localpro-iedn.onrender.com"});
-const api = axios.create({baseURL: "http://localhost:5000"});
+const api = axios.create({baseURL: "https://localpro-iedn.onrender.com"});
+// const api = axios.create({baseURL: "http://localhost:5000"});
 
 api.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
